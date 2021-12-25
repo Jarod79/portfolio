7b9cb148e9bbb9f9d3be7c60cfbc2182bbd1446d
jarod79 <arrijuria.eric@gmail.com>
2021-12-25 23:50:50 +0100

    Change some css

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./menu.css";
+import git from "../img/git.png";
+import linkedin from "../img/Linkedin.png";
 
 const Menu = () => {
   return (
     <div className="navigation" id="accueil">
       <div className="menu">
+        <a
+          href="https://www.linkedin.com/in/eric-arrijuria-a64990/"
+          target="blank"
+          className="menu_link_social"
+        >
+          <img src={linkedin} alt="Linkedin" className="linkedin" />
+        </a>
         <Link className="menu_link" to="/">
           ACCUEIL
         </Link>
@@ -21,6 +30,13 @@ const Menu = () => {
         <Link className="menu_link" to="/contact">
           CONTACT
         </Link>
+        <a
+          href="https://github.com/Jarod79"
+          target="blank"
+          className="menu_link_social"
+        >
+          <img src={git} alt="GitHub" className="git" />
+        </a>
       </div>
     </div>
   );
