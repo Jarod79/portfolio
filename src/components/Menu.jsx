869ccace61ff7e48b4f0695329202fe2f9407692
jarod79 <arrijuria.eric@gmail.com>
2021-12-28 17:40:36 +0100

    Responsive général

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,47 +9,45 @@ const Menu = ({ change }) => {
   const [burgerButton, setBurgerButton] = useState(false);
   return (
     <div className="navigation">
-      {!burgerButton && (
-        <div className={change ? "menu" : "menu_b"}>
-          <a
-            href="https://www.linkedin.com/in/eric-arrijuria-a64990/"
-            target="blank"
-            className="menu_link_social"
-          >
-            <img src={linkedin} alt="Linkedin" className="linkedin" />
-          </a>
-          <NavLink activeclassname="active" className="menu_link" to="/">
-            ACCUEIL
-          </NavLink>
-          <NavLink activeclassname="active" className="menu_link" to="/Apropos">
-            A PROPOS
-          </NavLink>
-          <NavLink
-            activeclassname="active"
-            className="menu_link"
-            to="/Competences"
-          >
-            COMPETENCES
-          </NavLink>
-          <NavLink
-            activeclassname="active"
-            className="menu_link"
-            to="/portfolio"
-          >
-            PORTFOLIO
-          </NavLink>
-          <NavLink activeclassname="active" className="menu_link" to="/contact">
-            CONTACT
-          </NavLink>
-          <a
-            href="https://github.com/Jarod79"
-            target="blank"
-            className="menu_link_social"
-          >
-            <img src={git} alt="GitHub" className="git" />
-          </a>
-        </div>
-      )}
+      <div
+        className={change ? "menu" : "menu_b"}
+        id={burgerButton ? "menu_show" : "menu_hide"}
+      >
+        <a
+          href="https://www.linkedin.com/in/eric-arrijuria-a64990/"
+          target="blank"
+          className="menu_link_social"
+        >
+          <img src={linkedin} alt="Linkedin" className="linkedin" />
+        </a>
+        <NavLink activeclassname="active" className="menu_link" to="/">
+          ACCUEIL
+        </NavLink>
+        <NavLink activeclassname="active" className="menu_link" to="/Apropos">
+          A PROPOS
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="menu_link"
+          to="/Competences"
+        >
+          COMPETENCES
+        </NavLink>
+        <NavLink activeclassname="active" className="menu_link" to="/portfolio">
+          PORTFOLIO
+        </NavLink>
+        <NavLink activeclassname="active" className="menu_link" to="/contact">
+          CONTACT
+        </NavLink>
+        <a
+          href="https://github.com/Jarod79"
+          target="blank"
+          className="menu_link_social"
+        >
+          <img src={git} alt="GitHub" className="git" />
+        </a>
+      </div>
+
       <button
         className={change ? "menu_button" : "menu_button_b"}
         onClick={() => setBurgerButton(!burgerButton)}
