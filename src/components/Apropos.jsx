9e7338ac187ea4322ddd1fe7adefe6372b19fa03
jarod79 <arrijuria.eric@gmail.com>
2022-01-06 23:33:41 +0100

    modification de texte

--- src/components/Apropos.jsx
+++ src/components/Apropos.jsx
@@ -13,12 +13,12 @@ const Apropos = ({ changeCss }) => {
         <p>
           Fan de nouvelles technologies, et aimant apprendre, j’ai décidé de
           faire de ma passion mon métier. Ayant toujours été attiré par le
-          métier de développeur web, et après une vingtaines d’années riches en
-          diverses expériences professionnelles, j’ai décidé de me reconvertir
-          dans le numérique. Actuellement en formation développeur web junior à
-          la Wild Code School, je souhaite par la suite poursuivre mon
-          apprentissage. Je suis donc à la recherche d’une alternance ou d’un
-          stage à partir de Février 2022.
+          métier de développeur web, et après une vingtaine d’années riches en
+          diverses expériences professionnelles, j’ai décidé de mettre à profit
+          mes compétences acquises pour me lancer dans le numérique.
+          Actuellement en formation développeur web à la Wild Code School, je
+          souhaite par la suite poursuivre mon apprentissage. Je suis donc à la
+          recherche d’une alternance ou d’un stage à partir du 13 Février 2022.
         </p>
       </div>
     </div>
